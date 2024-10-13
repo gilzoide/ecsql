09cb56de526083d0bb1d0864953cc3bdced1e9b4
gilzoide <gilzoide@gmail.com>
2024-10-13 11:50:29 -0300

    Move raylib components to draw_systems.hpp

--- src/systems/draw_systems.hpp
+++ src/systems/draw_systems.hpp
@@ -1,5 +1,59 @@
 #pragma once
 
+#include "../ecsql/component.hpp"
 #include "../ecsql/ecsql.hpp"
+
+inline ecsql::RawComponent PositionComponent {
+	"Position",
+	{
+		"x DEFAULT 0",
+		"y DEFAULT 0",
+		"z DEFAULT 0",
+	}
+};
+inline ecsql::RawComponent RotationComponent {
+	"Rotation",
+	{
+		"x DEFAULT 0",
+		"y DEFAULT 0",
+		"z DEFAULT 0",
+	}
+};
+inline ecsql::RawComponent ScaleComponent {
+	"Scale",
+	{
+		"x DEFAULT 1",
+		"y DEFAULT 1",
+		"z DEFAULT 1",
+	}
+};
+inline ecsql::RawComponent PivotComponent {
+	"Pivot",
+	{
+		"x DEFAULT 0.5",
+		"y DEFAULT 0.5",
+		"z DEFAULT 0.5",
+	}
+};
+
+inline ecsql::RawComponent ColorComponent {
+	"Color",
+	{
+		"r DEFAULT 255",
+		"g DEFAULT 255",
+		"b DEFAULT 255",
+		"a DEFAULT 255",
+	}
+};
+
+inline ecsql::RawComponent RectangleComponent {
+	"Rectangle",
+	{
+		"x DEFAULT 0",
+		"y DEFAULT 0",
+		"width DEFAULT 1",
+		"height DEFAULT 1",
+	}
+};
 
 void register_draw_systems(ecsql::Ecsql& world);
