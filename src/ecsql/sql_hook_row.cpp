d6a6b0e495efee2260169aebb5d0b1533cc1cbe7
gilzoide <gilzoide@gmail.com>
2024-10-16 18:16:05 -0300

    Update SQLHookRow to follow SQLRow API

--- src/ecsql/sql_hook_row.cpp
+++ src/ecsql/sql_hook_row.cpp
@@ -23,17 +23,31 @@ sqlite3_value *SQLHookRow::column_value(int index) const {
 bool SQLHookRow::column_bool(int index) const {
     return sqlite3_value_int(column_value(index));
 }
+
 int SQLHookRow::column_int(int index) const {
     return sqlite3_value_int(column_value(index));
 }
+
 sqlite3_int64 SQLHookRow::column_int64(int index) const {
     return sqlite3_value_int64(column_value(index));
 }
+
 double SQLHookRow::column_double(int index) const {
     return sqlite3_value_double(column_value(index));
 }
-const unsigned char *SQLHookRow::column_text(int index) const {
-    return sqlite3_value_text(column_value(index));
+
+std::string_view SQLHookRow::column_text(int index) const {
+	sqlite3_value *value = column_value(index);
+	const unsigned char *text = sqlite3_value_text(value);
+	int size = sqlite3_value_bytes(value);
+    return std::string_view((const char *) text, size);
+}
+
+std::span<const uint8_t> SQLHookRow::column_blob(int index) const {
+	sqlite3_value *value = column_value(index);
+	const uint8_t *data = (const uint8_t *) sqlite3_value_blob(value);
+	int size = sqlite3_value_bytes(value);
+	return std::span<const uint8_t>(data, size);
 }
 
 bool SQLHookRow::column_is_null(int index) const {
--- src/ecsql/sql_hook_row.hpp
+++ src/ecsql/sql_hook_row.hpp
@@ -1,15 +1,18 @@
 #pragma once
 
 #include <memory>
+#include <span>
 #include <string>
 #include <string_view>
 #include <tuple>
 #include <utility>
+#include <vector>
 
 #include <reflect>
 #include <sqlite3.h>
 
 #include "entity.hpp"
+#include "is_optional.hpp"
 
 namespace ecsql {
 
@@ -21,7 +24,8 @@ struct SQLHookRow {
     int column_int(int index) const;
     sqlite3_int64 column_int64(int index) const;
     double column_double(int index) const;
-    const unsigned char *column_text(int index) const;
+    std::string_view column_text(int index) const;
+	std::span<const uint8_t> column_blob(int index) const;
 
     bool column_is_null(int index) const;
 
@@ -42,7 +46,19 @@ protected:
     sqlite3 *db;
     bool use_new_row;
 
-    template<typename T> T get_advance(int& index) const {
+    template<typename T> T get_advance(int& index) const
+    requires is_optional<T>
+    {
+        if (column_is_null(index)) {
+            return std::nullopt;
+        }
+        else {
+            return get_advance<typename T::value_type>(index);
+        }
+    }
+    
+    template<typename T> T get_advance(int& index) const
+    requires (not is_optional<T>) {
         T value;
         reflect::for_each<T>([&](auto I) {
             auto&& field = reflect::get<I>(value);
@@ -99,23 +115,29 @@ protected:
     }
     
     template<> const unsigned char *get_advance(int& index) const {
-        return column_text(index++);
+        return (const unsigned char *) column_text(index++).data();
     }
     
     template<> const char *get_advance(int& index) const {
-        return (const char *) column_text(index++);
+        return (const char *) column_text(index++).data();
     }
     
     template<> std::string get_advance(int& index) const {
-        int size = sqlite3_value_bytes(column_value(index));
-        const char *text = get_advance<const char *>(index);
-        return std::string(text, size);
+		std::string_view text = column_text(index++);
+        return std::string(text);
     }
     
     template<> std::string_view get_advance(int& index) const {
-        int size = sqlite3_value_bytes(column_value(index));
-        const char *text = get_advance<const char *>(index);
-        return std::string_view(text, size);
+        return column_text(index++);
+    }
+    
+	template<> std::span<const uint8_t> get_advance(int& index) const {
+        return column_blob(index++);
+    }
+
+	template<> std::vector<uint8_t> get_advance(int& index) const {
+		std::span<const uint8_t> span = column_blob(index++);
+        return std::vector<uint8_t>(span.begin(), span.end());
     }
     
     template<> Entity get_advance(int& index) const {
