0684e6246f242ad179163e8aaa0aec4c3d62453b
gilzoide <gilzoide@gmail.com>
2024-10-25 07:56:38 -0300

    Add time.uptime

--- src/ecsql/time.hpp
+++ src/ecsql/time.hpp
@@ -5,11 +5,14 @@
 namespace ecsql {
 
 struct time {
+	// Schema
 	float delta;
+	float uptime;
 
-	inline static const char schema_sql[] = "CREATE TABLE time(delta)";
-	inline static const char insert_singleton_sql[] = "INSERT INTO time(delta) VALUES(0)";
-	inline static const char update_delta_sql[] = "UPDATE time SET delta = ?";
+	// SQL statements
+	inline static const char schema_sql[] = "CREATE TABLE time(delta, uptime)";
+	inline static const char insert_singleton_sql[] = "INSERT INTO time(delta, uptime) VALUES(0, 0)";
+	inline static const char update_delta_sql[] = "UPDATE time SET delta = ?1, uptime = uptime + ?1";
 };
 
 }
