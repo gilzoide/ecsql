d35ef518be63157e9740ca324812a7f5383ccb2a
gilzoide <gilzoide@gmail.com>
2025-02-01 15:35:32 -0300

    Remove ScreenRect system

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,6 @@
 #include "scripting/lua_scripting.hpp"
 #include "systems/draw_systems.hpp"
 #include "systems/key_handler.hpp"
-#include "systems/screen_rect.hpp"
 #include "systems/yoga.hpp"
 
 #if defined(DEBUG) && !defined(NDEBUG)
@@ -108,7 +107,6 @@ int main(int argc, const char **argv) {
 	world.execute_sql_script(final_schema);
 
 	// Systems
-	register_update_screen_rect(world);
 	register_key_handler(world);
 	register_update_yoga(world);
 	register_draw_systems(world);
