031aa621fcee05fbfb42554d86190ae597133bf9
gilzoide <gilzoide@gmail.com>
2025-08-06 09:24:42 -0300

    Compress texture atlas XML when zipping
    This is a micro-optimization in build size, but it's a first take in asset build processing

--- tools/pack_assets.py
+++ tools/pack_assets.py
@@ -2,7 +2,28 @@
 Pack assets into a ZIP file, transforming them if necessary.
 """
 import os
+import re
 from zipfile import ZipFile, ZIP_DEFLATED
+
+
+def process_texture_atlas(path: str) -> str:
+    """
+    Compacts texture atlas XML files.
+    Our parser ignores XML format and simply reads the attributes in order.
+    """
+    assert(os.path.exists(os.path.splitext(path)[0] + ".png"))
+    contents = []
+    wanted_content = re.compile(r'name=[^/]+')
+    with open(path, "r") as f:
+        for line in f:
+            if m := wanted_content.search(line):
+                contents.append(m.group(0))
+    return "\n".join(contents)
+
+
+ASSET_PROCESSOR = {
+    ".xml": process_texture_atlas,
+}
 
 
 def pack_assets(assets_folder: str, zipname: str):
@@ -14,7 +35,12 @@ def pack_assets(assets_folder: str, zipname: str):
                     continue
                 filepath = os.path.join(dirname, filename)
                 archivepath = os.path.relpath(filepath, assets_folder)
-                zipfile.write(filepath, archivepath)
+                ext = os.path.splitext(filepath)[1]
+                if f := ASSET_PROCESSOR.get(ext):
+                    contents = f(filepath)
+                    zipfile.writestr(archivepath, contents)
+                else:
+                    zipfile.write(filepath, archivepath)
 
 
 if __name__ == "__main__":
